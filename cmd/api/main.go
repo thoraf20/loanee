a05409670dfc463aab63cca63060b50b60b75359
Toheeb-mida <tayinde@getmida.com>
2025-10-06 14:22:27 +0100

    setup postgres db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,16 +5,22 @@ import (
 	"log"
 	"net/http"
 
+	dbConfig "github.com/thoraf20/loanee/internal/config"
 	router "github.com/thoraf20/loanee/internal/http"
 )
 
 func main() {
+	cfg, err := dbConfig.LoadConfig()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
 	router := router.NewRouter()
 
-	port := ":8080"
-	fmt.Printf("Server running on http://localhost%s\n", port)
+	addr := fmt.Sprintf(":%s", cfg.AppPort)
+	fmt.Printf("[%s] Server running on http://localhost%s\n", cfg.AppEnv, addr)
 
-	if err := http.ListenAndServe(port, router); err != nil {
+	if err := http.ListenAndServe(addr, router); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
 }
---
+++ internal/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	AppEnv     string
+	AppPort    string
+	DBHost     string
+	DBPort     int
+	DBUser     string
+	DBPassword string
+	DBName     string
+	JWTSecret  string
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Printf("No .env file found, reading from environment variables")
+	}
+
+	cfg := &Config{
+		AppEnv:     viper.GetString("APP_ENV"),
+		AppPort:    viper.GetString("APP_PORT"),
+		DBHost:     viper.GetString("DB_HOST"),
+		DBPort:     viper.GetInt("DB_PORT"),
+		DBUser:     viper.GetString("DB_USER"),
+		DBPassword: viper.GetString("DB_PASSWORD"),
+		DBName:     viper.GetString("DB_NAME"),
+		JWTSecret:  viper.GetString("JWT_SECRET"),
+	}
+
+	return cfg, nil
+}
+
+func (c *Config) GetDBURL() string {
+	return fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+		c.DBHost, c.DBUser, c.DBPassword, c.DBName, c.DBPort,
+	)
+}
